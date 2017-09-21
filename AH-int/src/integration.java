6a38ae7d2beb600e8ad0925ba1d256ec04daace8
ajhoward9982 <ajhoward9982@H339R2S1.STUDENT.ad.fgcu.edu>
2017-09-21 10:07:07 -0400

    Trying some stuff

--- AH-int/src/integration.java
+++ AH-int/src/integration.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 /**
  * 
  */
 
 /**
  * @author ajhoward9982
+ * 
+ * Andrew Howard
+ * 
+ * Im not sure yet what exactly this is going to do but it should be cool.
  *
  */
 public class integration {
@@ -11,9 +17,13 @@ public class integration {
 	/**
 	 * @param args
 	 */
+	
 	public static void main(String[] args) {
 		
-
+		System.out.print("Type in the name of the month followed by the date.");
+		//Scanner sc = new(Scanner(System.in))
+		//string month = sc.nextString()
+		//This wont work for a bit
 	}
 
 }
