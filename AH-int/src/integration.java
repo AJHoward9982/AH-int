34aa148aef4ed749c2f823d9aa049f754962f2fb
ajhoward9982 <ajhoward9982@H339R2S1.STUDENT.ad.fgcu.edu>
2017-09-28 10:07:44 -0400

    9/28 additions

--- AH-int/src/integration.java
+++ AH-int/src/integration.java
@@ -1,29 +1,35 @@
 import java.util.Scanner;
-
 /**
- * 
- */
-
-/**
- * @author ajhoward9982
- * 
  * Andrew Howard
  * 
- * Im not sure yet what exactly this is going to do but it should be cool.
+ * Basketball stats program
  *
  */
 public class integration {
 
+	public Scanner scan; {
 	/**
 	 * @param args
 	 */
+	}
 	
 	public static void main(String[] args) {
 		
-		System.out.print("Type in the name of the month followed by the date.");
-		//Scanner sc = new(Scanner(System.in))
-		//string month = sc.nextString()
-		//This wont work for a bit
-	}
+		demoMethods();   //This is a call, whats inside the parenthesis of a call is an argument.
 
+	}
+	
+	public static void demoMethods() {
+		
+		int num1 = 10;
+		int num2 = 6;
+		double result = (double) num1 / num2;
+				
+		System.out.println("Hello World! " + result ); // demo of main calling a method, adding numbers
+													   // concating strings, and printing.
+		
+	}
+	public static void Player() {
+		
+	}
 }
