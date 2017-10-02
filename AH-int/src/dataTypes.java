00b4170c27e678cf0fc51323efe91c0a860c501a
trobi <trobi@10.0.0.18>
2017-10-01 20:19:12 -0400

    Lots of new stuff

---
+++ AH-int/src/dataTypes.java
@@ -0,0 +1,36 @@
+import javafx.scene.chart.PieChart.Data;
+
+public class dataTypes { 
+	
+	/*Using the public header on both this class and the method
+	Allow it to be used in the main method in my integration project
+	by using a call*/
+	
+	public void numbers(){
+		
+		/*The following are variables which are positions in memory that can store Data
+		The data stored is defined by the type of variable such as "byte , short , int, etc"
+		These variables can then be used by opperators and other lines of code to manipulate
+		and use them.*/
+		
+		byte A = 5;  //8 bit number storage
+		short B = 32000; //16 bit number storage
+		int C = 9999999; //32 bit number storage
+		long D = 10000000; //64 bit number storage
+		float E = (float) 1.412; //stores floating pint numbers, less accurate than double
+		double F = (double) 1.43235; //accurate storage of decimals. Used commonly for money
+		boolean G = true & false; //boolean logic storage    true/false
+		char H = 'A' ;
+		String I = "A string is a complex data type";
+		
+		System.out.println( A );
+		System.out.println( B );
+		System.out.println( C );
+		System.out.println( D );
+		System.out.println( E );
+		System.out.println( F );
+		System.out.println( G );
+		System.out.println( H );
+		System.out.println( I );
+	}
+}
--- AH-int/src/integration.java
+++ AH-int/src/integration.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+
+import com.sun.org.apache.bcel.internal.classfile.Method;
 /**
  * Andrew Howard
  * 
@@ -15,8 +17,27 @@ public class integration {
 	
 	public static void main(String[] args) {
 		
-		demoMethods();   //This is a call, whats inside the parenthesis of a call is an argument.
-
+		demoMethods();   //This is a call, what's inside the parenthesis of a call is an argument.
+		
+		dataDemo();	// Calling my data type demo
+		
+	}
+	
+	public static void dataDemo() {
+		
+		dataTypes dataTypesObject = new dataTypes();
+		dataTypesObject.numbers();
+		
+		
+	}
+	
+	public static int add(int a , int b) {
+		
+		/*A method to add 2 numbers together, "int a" and "int b" are parameters
+		for when the method is called.*/
+		
+		int sum = a + b;
+		return sum;
 	}
 	
 	public static void demoMethods() {
@@ -25,11 +46,19 @@ public class integration {
 		int num2 = 6;
 		double result = (double) num1 / num2;
 				
-		System.out.println("Hello World! " + result ); // demo of main calling a method, adding numbers
-													   // concating strings, and printing.
+		System.out.println("Hello World! " + result ); 
 		
-	}
-	public static void Player() {
+		/*demo of main calling a method, adding numbers 
+		concating strings, and printing.*/
 		
+		int n1n2 = add( num1 , num2); //n1n2 is num1 + num2
+		
+		/*This calls the previously mentioned "add" Method. "num1" and "num2" are arguments
+		that will fit into the "int a" and "int b" parameters of add(int a, int b)*/
+		
+		System.out.println( "Adding 10 and 6 gives you " + n1n2 );
+	
 	}
+	
 }
+ 
