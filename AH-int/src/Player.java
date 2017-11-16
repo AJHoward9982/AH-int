5e4de3abfbe7a02b5fa4dcfa1feb6cb0cb3869fd
ajhoward9982 <ajhoward9982@H339R2S1.STUDENT.ad.fgcu.edu>
2017-11-16 10:04:39 -0500

    Trying to add game

--- AH-int/src/Player.java
+++ AH-int/src/Player.java
@@ -1,28 +1,29 @@
 public class Player {
-	String playerName;
-	int pNumber; //Jersey number
-	int pScored; //points scored
-	int steals;
-	int blocks;    // all will be used to calculate score
-	int rebounds;
-	int fouls; //fouls made
-	
-	Player(String playerName, int pNumber, int pScored, int steals, int blocks, int rebounds, int fouls) {
-		this.playerName = playerName;
-		this.pNumber = pNumber;
-		this.pScored = pScored;
-		this.steals = steals;
-		this.blocks = blocks;
-		this.rebounds = rebounds;
-		this.fouls = fouls;
-		
-	}
-		
-	void playerScore() {
-		int score = (pScored + steals + blocks + rebounds) - fouls;
-		
-		System.out.println( playerName + " has a score of " + score + " and wears number " + pNumber);
-		
-	}
-}
+  String playerName;
+  int pNumber; // Jersey number
+  int pScored; // points scored
+  int steals;
+  int blocks; // all will be used to calculate score
+  int rebounds;
+  int fouls; // fouls made
 
+  Player(String playerName, int pNumber, int pScored, int steals, int blocks,
+      int rebounds, int fouls) {
+    this.playerName = playerName;
+    this.pNumber = pNumber;
+    this.pScored = pScored;
+    this.steals = steals;
+    this.blocks = blocks;
+    this.rebounds = rebounds;
+    this.fouls = fouls;
+
+  }
+
+  void playerScore() {
+    int score = (pScored + steals + blocks + rebounds) - fouls;
+
+    System.out.println(playerName + " has a score of " + score
+        + " and wears number " + pNumber);
+
+  }
+}
--- AH-int/src/integration.java
+++ AH-int/src/integration.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Scanner;
+
 /**
  * Andrew Howard
  * 
@@ -10,219 +11,240 @@ import java.util.Scanner;
 
 public class integration {
 
-	static Boolean cont = true;
-	
-	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);
-		
-		int[] arr = { 10 , 42 , 52 , 14 , 21 };
-		
-		Player player1 = new Player("Kyrie" , 11 , 25 , 3 , 4 , 11 , 3);
-		Player player2 = new Player("Rondo" , 9 , 13 , 7 , 4 , 8 , 5);
-		Player player3 = new Player("Shaq" , 34 , 20 , 0 , 7 , 15 , 4);
-		
-		ArrayList<Player> players = new ArrayList<Player>(); //Array List of Players
-		
-		players.add(player1);
-		players.add(player2);
-		players.add(player3);
-		
-		if (cont == true) {
-			
-			System.out.println("Select a letter: ");
-			System.out.println("a: Run the data type demo");
-			System.out.println("b: Show the stored players");
-			System.out.println("c: Display a players' data");
-			System.out.println("d: Input a number and it will be counted back to you");
-			System.out.println("e: Array demo");
-			System.out.println("f: HashMap demo");
-			System.out.println("f: Operators demo");
-			System.out.println("Type 'Quit' to stop the program");
-			
-			String caseVal = scan.nextLine(); //Which case to run
-			
-			switch (caseVal) {
-				
-				case "a" : 
-					dataDemo();
-					break;
-				
-				case "b" : 
-					System.out.println( player1.playerName );
-					System.out.println( player2.playerName );
-					System.out.println( player3.playerName );
-					break;
-				
-				case "c" :
-					
-					Iterator itr = players.iterator();
-					while(itr.hasNext()) {
-						Player player = (Player)itr.next();
-						
-						System.out.println( player.playerName + "'s number is " + player.pNumber + ".");
-						System.out.println( player.playerName + " scored " + player.pScored + "points.");
-						System.out.println( player.playerName + " had " + player.steals + " steals.");
-						System.out.println( player.playerName + " had " + player.blocks + " blocks.");
-						System.out.println( player.playerName + " had " + player.rebounds + " rebounds.");
-						System.out.println( player.playerName + " had " + player.fouls + " fouls.");
-						System.out.println("\n");
-					}
-					break;
-				
-				case "d" : 
-					System.out.println("Enter a number:");
-					
-					int times = scan.nextInt();
-					
-					for ( int i = 0; i < times ; i++) {
-						System.out.print( i + 1) ;
-						if ( i < (times - 1)) {
-							System.out.print(" , ");
-						}
-					}
-					break;
-				
-				case "e" : 
-					
-					sort(arr);
-					
-					System.out.println("The sorted list is");
-					
-					for(int i=0; i < arr.length; i++) {
-						System.out.print(arr[i] + "  ");
-					}
-					
-					System.out.println("The sum is " + sum(arr) );
-					
-					break;
-				
-				case "f" : 
-					hashMapDemo();
-					break;
-				
-				case "g" : 
-					operators();
-					break;
-					
-				case "Quit" : 
-					cont = false;
-					break;
-				
-				default : 
-					System.out.println("Inpur error: Try again");
-					break;
-					
-			}
-			System.out.println("\n \n ");
-			main( args );
-		}
-		
+  static Boolean cont = true;
 
-		/*
-		Player kIrving = new Player();
-		kIrving.playerName = "Kyrie Irving";
-		kIrving.pNumber = 11;
-		kIrving.pScored = 25;
-		kIrving.steals = 3;
-		kIrving.blocks = 4;
-		kIrving.rebounds = 11;
-		kIrving.fouls =	3;
-		*/
-	}
-	
-	public static void dataDemo() {
-		
-		dataTypes dataTypesObject = new dataTypes();
-		dataTypesObject.numbers();
-		
-		
-	}
-	
-	public static void hashMapDemo() {
-		HashMap<Integer, Boolean> question = new HashMap<>();
-		
-		question.put(1, true);
-		question.put(2, false);
-		question.put(3, false);
-		question.put(4, true);
-		question.put(5, false);
-		
-		System.out.println( "The answer to question one is " + question.get(1) );
-		System.out.println( "The answer to question two is " + question.get(2) );
-		System.out.println( "The answer to question three is " + question.get(3) );
-		System.out.println( "The answer to question four is " + question.get(4) );
-		System.out.println( "The answer to question five is " + question.get(5) );
-			
-	}
-	
-	public static int add(int a , int b) {
-		
-		/*A method to add 2 numbers together, "int a" and "int b" are parameters
-		for when the method is called.*/
-		
-		int sum = a + b;
-		return sum;
-	}
-	
-	public static void operators() {
-		boolean Value1 = ( 4 < 5 );
-		boolean Value2 = ( 4 == 5 );
-		System.out.println("Statement one that 4 < 5 is " + Value1);
-		System.out.println("Statement two that 4 = 5 is " + Value2);
-	}
-	
-	public static int sum(int[] arr) {
-		
-		int sum = 0;
-		
-		for(int i=0; i < (arr.length); i++ ) {
-			sum += arr[i];
-		}
-		return sum;
-	}
-	
-	
-	public static int[] sort(int[] arr) {
-		for(int i=0; i < (arr.length - 1); i++ ) {
-			
-			for( int j = 0; j < arr.length - 1 - i; j++) {
-				
-				if ( arr[j] > arr[j+1] ) {
+  public static void main(String[] args) {
 
-					int t = arr[j]; //Holds the value of "i+1" so that "i" can replace it
-					
-					arr[j] = arr[j + 1];
-					
-					arr[j + 1] = t;
-	
-				}
-			
-			}
-		}
+    Scanner scan = new Scanner(System.in);
 
-		return arr;
-	}
-	
-	public static void demoMethods() {
-		
-		int num1 = 10;
-		int num2 = 6;
-		double result = (double) num1 / num2;
-				
-		System.out.println("Hello World! " + result ); 
-		
-		/*demo of main calling a method, adding numbers 
-		concating strings, and printing.*/
-		
-		int n1n2 = add( num1 , num2); //n1n2 is num1 + num2
-		
-		/*This calls the previously mentioned "add" Method. "num1" and "num2" are arguments
-		that will fit into the "int a" and "int b" parameters of add(int a, int b)*/
-		
-		System.out.println( "Adding 10 and 6 gives you " + n1n2 );
-	
-	}
-	
+    int[] arr = { 10, 42, 52, 14, 21 };
+    
+    int[][] arr2D = new int[3][3];
+
+    Player player1 = new Player("Kyrie", 11, 25, 3, 4, 11, 3);
+    Player player2 = new Player("Rondo", 9, 13, 7, 4, 8, 5);
+    Player player3 = new Player("Shaq", 34, 20, 0, 7, 15, 4);
+
+    ArrayList<Player> players = new ArrayList<Player>(); //Array List of Players
+
+    players.add(player1);
+    players.add(player2);
+    players.add(player3);
+
+    while (cont == true) {
+
+      System.out.println("Select a letter: ");
+      System.out.println("a: Run the data type demo");
+      System.out.println("b: Show the stored players");
+      System.out.println("c: Display a players' data");
+      System.out
+          .println("d: Input a number and it will be counted back to you");
+      System.out.println("e: Array demo");
+      System.out.println("f: HashMap demo");
+      System.out.println("g: Operators demo");
+      System.out.println("x: Experimental");
+      System.out.println("Type 'Quit' to stop the program");
+
+      String caseVal = scan.nextLine(); // Which case to run
+
+      switch (caseVal) {
+
+        case "a":
+          dataDemo();
+          break;
+
+        case "b":
+          System.out.println(player1.playerName);
+          System.out.println(player2.playerName);
+          System.out.println(player3.playerName);
+          break;
+
+        case "c":
+
+          Iterator itr = players.iterator();
+          while (itr.hasNext()) {
+            Player player = (Player) itr.next();
+
+            System.out.println(
+                player.playerName + "'s number is " + player.pNumber + ".");
+            System.out.println(
+                player.playerName + " scored " + player.pScored + "points.");
+            System.out.println(
+                player.playerName + " had " + player.steals + " steals.");
+            System.out.println(
+                player.playerName + " had " + player.blocks + " blocks.");
+            System.out.println(
+                player.playerName + " had " + player.rebounds + " rebounds.");
+            System.out.println(
+                player.playerName + " had " + player.fouls + " fouls.");
+            System.out.println("\n");
+          }
+          break;
+
+        case "d":
+          System.out.println("Enter a number:");
+
+          int times = scan.nextInt();
+
+          for (int i = 0; i < times; i++) {
+            System.out.print(i + 1);
+            if (i < (times - 1)) {
+              System.out.print(" , ");
+            }
+          }
+          break;
+
+        case "e":
+
+          sort(arr);
+
+          System.out.println("\n" + "The sorted list is:");
+
+          for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "  ");
+          }
+
+          System.out.println("\n\nThe sum is " + sumClass(arr));
+          
+          //Add 2D Array
+          
+          System.out.println("\n" + arr2D[1][0]);
+
+          break;
+
+        case "f":
+          hashMapDemo();
+          break;
+
+        case "g":
+          operators();
+          break;
+         
+        case "x":
+          gameAttempt();
+          break;
+
+        case "Quit":
+          cont = false;
+          break;
+
+        default:
+          System.out.println("Input error: Try again");
+          break;
+
+      }
+      System.out.println("\n \n ");
+      main(args);
+    }
+
+  }
+
+  public static void dataDemo() {
+
+    dataTypes dataTypesObject = new dataTypes();
+    dataTypesObject.numbers();
+
+  }
+  
+  public static void printGrid() {
+  
+  }
+  
+  public static void gameAttempt() {
+    
+    int[][] grid = new int[5][5];
+    
+  }
+
+  public static void hashMapDemo() {
+    HashMap<Integer, Boolean> question = new HashMap<>();
+
+    question.put(1, true);
+    question.put(2, false);
+    question.put(3, false);
+    question.put(4, true);
+    question.put(5, false);
+
+    System.out.println("The answer to question one is " + question.get(1));
+    System.out.println("The answer to question two is " + question.get(2));
+    System.out.println("The answer to question three is " + question.get(3));
+    System.out.println("The answer to question four is " + question.get(4));
+    System.out.println("The answer to question five is " + question.get(5));
+
+  }
+
+  public static int add(int a, int b) {
+
+    /*
+     * A method to add 2 numbers together, "int a" and "int b" are parameters
+     * for when the method is called.
+     */
+
+    int sumOpp = a + b;
+    return sumOpp;
+  }
+
+  public static void operators() {
+    boolean Value1 = (4 < 5);
+    boolean Value2 = (4 == 5);
+    System.out.println("Statement one that 4 < 5 is " + Value1);
+    System.out.println("Statement two that 4 = 5 is " + Value2);
+  }
+
+  public static int sumClass(int[] arr) {
+
+    int ans = 0;
+
+    for (int i = 0; i < (arr.length); i++) {
+      ans += arr[i];
+    }
+    return ans;
+  }
+
+  public static int[] sort(int[] arr) {
+    for (int i = 0; i < (arr.length - 1); i++) {
+
+      for (int j = 0; j < arr.length - 1 - i; j++) {
+
+        if (arr[j] > arr[j + 1]) {
+
+          int t = arr[j]; // Holds the value of "i+1" so that "i" can replace it
+
+          arr[j] = arr[j + 1];
+
+          arr[j + 1] = t;
+
+        }
+
+      }
+    }
+
+    return arr;
+  }
+
+  public static void demoMethods() {
+
+    int num1 = 10;
+    int num2 = 6;
+    double result = (double) num1 / num2;
+
+    System.out.println("Hello World! " + result);
+
+    /*
+     * demo of main calling a method, adding numbers concating strings, and
+     * printing.
+     */
+
+    int n1n2 = add(num1, num2); // n1n2 is num1 + num2
+
+    /*
+     * This calls the previously mentioned "add" Method. "num1" and "num2" are
+     * arguments that will fit into the "int a" and "int b" parameters of
+     * add(int a, int b)
+     */
+
+    System.out.println("Adding 10 and 6 gives you " + n1n2);
+
+  }
+
 }
- 
